ed61681e0f13218961ccaf50045d8ba86ea3e49c
mechtifs <mechtifs@gmail.com>
2023-02-14 18:20:44 +0800

    update

--- src/extension.js
+++ src/extension.js
@@ -135,13 +135,10 @@ class DesktopLyric {
 
     set index(index) {
         if(this._index === index) return;
-        if(!index) Main.panel.statusArea.appMenu.connectObject('changed', () => {
-                Main.panel.statusArea.appMenu._visible ? Main.panel.statusArea.appMenu.show() : Main.panel.statusArea.appMenu.hide();
-            }, this);
-        else Main.panel.statusArea.appMenu.disconnectObject(this);
         this._index = index;
         this.systray = false;
         this.systray = true;
+        this.appMenuHidden = !index & this._showing;
     }
 
     set view(view) {
@@ -169,6 +166,16 @@ class DesktopLyric {
         this._showing = !closed;
         if(closed) this.status = 'Stopped';
         if(this._button) this._button.visible = !closed;
+        this.appMenuHidden = !this._index & !closed;
+    }
+
+    set appMenuHidden(appMenuHidden) {
+        if(this._appMenuHidden === appMenuHidden) return;
+        this._appMenuHidden = appMenuHidden;
+        if(appMenuHidden) Main.panel.statusArea.appMenu.connectObject('changed', () => {
+                Main.panel.statusArea.appMenu._visible ? Main.panel.statusArea.appMenu.show() : Main.panel.statusArea.appMenu.hide();
+            }, this);
+        else Main.panel.statusArea.appMenu.disconnectObject(this);
     }
 
     syncPosition(cb) {
@@ -244,7 +251,7 @@ class DesktopLyric {
         this._field.detach(this);
         this.systray = this.playing = null;
         Main.overview.disconnectObject(this);
-        if(!this._index) Main.panel.statusArea.appMenu.disconnectObject(this);
+        this.appMenuHidden = false;
         ['_mpris', '_lyric', '_paper'].forEach(x => { this[x]?.destroy(); this[x] = null; });
     }
 }
